6ad1a154dab047540937d0dfbf2f700d9c480578
chiwingsit <chiwingsit@gmail.com>
2015-11-28 23:55:50 -0500

    Add sequential simulated annealing

--- tsp_sequential.cpp
+++ tsp_sequential.cpp
@@ -4,13 +4,22 @@
 #include <iostream>
 #include <algorithm>    // std::next_permutation
 #include <sys/time.h>
+#include <math.h>
 
 using namespace std;
+
+double acceptanceProbability(double currentDistance, double newDistance, double temperature){
+    if(newDistance < currentDistance){
+        return 1.0;
+    }
+    return exp((currentDistance-newDistance)/temperature);
+}
 
 int main(int argc, char* argv[]) {
 
     struct timeval start, end;
     gettimeofday(&start, NULL);
+    srand (time(NULL));
 
     Graph graph = Graph(argv[1]);
     //graph.printDistanceMatrix();
@@ -22,19 +31,66 @@ int main(int argc, char* argv[]) {
 
     double distance = graph.getPathDistance(path);
     cout<< "Default distance: " << distance <<endl;
-    while(next_permutation(path, path + graph.getSize())){
+    // while(next_permutation(path, path + graph.getSize())){
         
-        double new_distance = graph.getPathDistance(path);
-        if(new_distance < distance)
-        {
-            distance = new_distance;
+    //     double newDistance = graph.getPathDistance(path);
+    //     if(newDistance < distance)
+    //     {
+    //         distance = newDistance;
             
+    //         cout << "Path: ";
+    //         for(int i = 0; i < graph.getSize(); i++){
+    //             cout << path[i] << ' ';
+    //         }
+    //         cout << endl << "Distance: " << distance << endl;
+    //     }
+    // }
+
+    double temperature = 100000;
+    double coolingRate = 0.0001;
+    int bestPath[graph.getSize()];
+    int currentPath[graph.getSize()];
+
+    for(int i = 0; i < graph.getSize(); i++){
+        bestPath[i] = i;
+        currentPath[i] = i;
+    }
+
+    double currentDistance = distance;
+
+    while(temperature > 0.001){
+
+        for(int i = 0; i < graph.getSize(); i++){
+            path[i] = currentPath[i];
+        }
+
+        int index1 = rand() % graph.getSize();
+        int index2 = rand() % graph.getSize();
+
+        int tmp = path[index1];
+        path[index1] = path[index2];
+        path[index2] = tmp;
+        
+        double newDistance = graph.getPathDistance(path);
+        //cout << acceptanceProbability(currentDistance, newDistance, temperature) << endl;
+        if(acceptanceProbability(currentDistance, newDistance, temperature) > rand()/RAND_MAX){
+            currentDistance = newDistance;
+            for(int i = 0; i < graph.getSize(); ++i){
+                currentPath[i] = path[i];
+            }
+        }
+
+        if(distance > currentDistance){
+            distance = currentDistance;
             cout << "Path: ";
-            for(int i = 0; i < graph.getSize(); i++){
-                cout << path[i] << ' ';
+            for(int i = 0; i < graph.getSize(); ++i){
+                bestPath[i] = currentPath[i];
+                cout << bestPath[i] << ' ';
             }
             cout << endl << "Distance: " << distance << endl;
         }
+
+        temperature *= 1-coolingRate;
     }
 
     gettimeofday(&end, NULL);
